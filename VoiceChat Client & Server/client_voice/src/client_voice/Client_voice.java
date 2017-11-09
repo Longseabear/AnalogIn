e5de2e97f5d0d0f02e9e734d9174ec4c3204bf27
Jason Jung <hyeonsuns@naver.com>
2017-11-09 17:49:56 +0900

    Voice Chatting (Client & Server)
    Voice Chat through Client and server architecture ( Multithreading )

---
+++ VoiceChat Client & Server/client_voice/src/client_voice/Client_voice.java
@@ -0,0 +1,9 @@
+package client_voice;
+
+public class Client_voice {
+	public static boolean calling = false;
+	public static void main(String[] args) {
+		client_fr fr = new client_fr();
+		fr.setVisible(true);
+	}
+}
---
+++ VoiceChat Client & Server/client_voice/src/client_voice/recorder_thread.java
@@ -0,0 +1,34 @@
+package client_voice;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.*;
+
+import javax.sound.sampled.TargetDataLine;
+
+public class recorder_thread extends Thread{
+	public TargetDataLine audio_in = null;
+	public DatagramSocket dout;
+	byte byte_buff[] = new byte[512];
+	public InetAddress server_ip;
+	public int server_port;
+	
+	@Override
+	public void run() {
+		int num = 1;
+		while(Client_voice.calling) {
+			try {
+				audio_in.read(byte_buff, 0, byte_buff.length);
+				DatagramPacket data = new DatagramPacket(byte_buff, byte_buff.length, server_ip, server_port);
+				System.err.println("send #"+ (num++));
+				dout.send(data);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				Logger.getLogger(recorder_thread.class.getName()).log(Level.SEVERE, null, e);
+			}
+		}
+		audio_in.close();
+		audio_in.drain();
+		System.out.println("Thread Stop");
+	}
+}
