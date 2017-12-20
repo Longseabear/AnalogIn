afc054f50a4c6c032446104c364173d459d4a8c9
Jason Jung <hyeonsuns@naver.com>
2017-12-20 21:22:44 +0900

    Modify Voice chat

--- Analogin/src/AnalogInProject/Client_voice.java
+++ Analogin/src/AnalogInProject/Client_voice.java
@@ -2,8 +2,4 @@ package AnalogInProject;
 
 public class Client_voice {
 	public static boolean calling = false;
-	public static void main(String[] args) {
-		client_fr fr = new client_fr();
-		fr.setVisible(true);
-	}
 }
--- Analogin/src/AnalogInProject/player_thread.java
+++ Analogin/src/AnalogInProject/player_thread.java
@@ -1,26 +1,31 @@
 package AnalogInProject;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.sound.sampled.SourceDataLine;
+import java.util.HashSet;
 
 public class player_thread extends Thread{
 	public DatagramSocket din;
 	public SourceDataLine audio_out;
 	byte[] buffer = new byte[512];
-	
+	private static HashSet<DatagramSocket> writers = new HashSet<DatagramSocket>();
 	@Override
 	public void run() {
+		writers.add(din);
 		DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
 		while(Server_voice.calling) {
 			try {
-				din.receive(incoming);
-				buffer = incoming.getData();
-				audio_out.write(buffer, 0, buffer.length);
+				for(DatagramSocket writer : writers) {
+					writer.receive(incoming);
+					buffer = incoming.getData();
+					audio_out.write(buffer, 0, buffer.length);
+				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				Logger.getLogger(player_thread.class.getName()).log(Level.SEVERE, null, e);
